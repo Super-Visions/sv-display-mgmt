96b671f1dfeb835f26df1741f70ebdde72cd8809
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2020-09-28 15:15:51 +0200

    Fixed Dutch translation

--- module.sv-display-mgmt.php
+++ module.sv-display-mgmt.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'sv-display-mgmt/1.0.0',
+	'sv-display-mgmt/1.0.1',
 	array(
 		// Identification
 		//
