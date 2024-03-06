6884a3584539f70a7fd704d6f0a1116619a37ab2
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2024-03-06 18:03:57 +0100

    PHP 8.1 compatibility
    This to workaround the bug mentioned in https://github.com/Combodo/iTop/pull/600

--- module.sv-display-mgmt.php
+++ module.sv-display-mgmt.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'sv-display-mgmt/1.1.0',
+	'sv-display-mgmt/1.1.1',
 	array(
 		// Identification
 		//
